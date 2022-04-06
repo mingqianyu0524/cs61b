d7e29aa7b79cc82afc4386b8aa3c6070e1fa1ed2
mingqian <yumingqian1993@yahoo.com>
2022-04-06 11:11:21 +0800

    done with timing bad resizing strategy

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,4 +1,5 @@
 package timingtest;
+
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -23,5 +24,22 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        for (int i = 1000; i < 256000; i = i * 2) {
+            AList<Integer> items = new AList<>();
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < i; j++) {
+                items.addLast(j);
+            }
+            times.addLast(sw.elapsedTime());
+            Ns.addLast(i);
+            opCounts.addLast(i);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 }
